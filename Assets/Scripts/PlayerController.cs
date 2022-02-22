55c703ace74ad0bdd399bbbd5adbb7d8da7fe8ee
darwageddon <canberk.ertrn@hotmail.com>
2022-02-23 00:28:58 +0300

    Setting the Player's Elbowroom
    The player’s position was set with a fixed X location to prevent the player from going off the side of the screen with an if-then statement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,10 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   if(transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
         horizontallInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontallInput * Time.deltaTime * speed);
     }
