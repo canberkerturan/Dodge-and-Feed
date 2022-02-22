07e4d3f847f3abf2f9436bc8bdac6008a8bd97a1
darwageddon <canberk.ertrn@hotmail.com>
2022-02-23 00:36:49 +0300

    Code Cleaning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontallInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +15,14 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   if(transform.position.x < -10)
+    {   if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontallInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontallInput * Time.deltaTime * speed);
