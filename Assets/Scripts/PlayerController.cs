02b4a78cf21184c63b13cb3ae38a52c5541b92e7
darwageddon <canberk.ertrn@hotmail.com>
2022-02-22 23:34:18 +0300

    Position fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontallInput;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        horizontallInput = Input.GetAxis("Horizontal");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontallInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontallInput * Time.deltaTime * speed);
     }
 }
