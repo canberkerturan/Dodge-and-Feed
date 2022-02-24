c7493ac104941a1d1f86de17caf5550c61460cfd
darwageddon <canberk.ertrn@hotmail.com>
2022-02-24 21:45:55 +0300

    Spawn Position Randomization
    New codes were added to randomize their spawn position, so they don’t march down the screen in a straight line.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnRangeZ = 20; 
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +20,9 @@ public class SpawnManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S))
         {
             int animalindex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-20, 20), 0, 20);
 
-            Instantiate(animalPrefabs[animalindex], new Vector3(0, 0, 20), animalPrefabs[animalindex].transform.rotation);
+            Instantiate(animalPrefabs[animalindex], spawnPos, animalPrefabs[animalindex].transform.rotation);
         }
     }
 }
